57fcd65b773fd40b418cddf9143f98c9ace519ad
llvllch <mart@llvll.ch>
2021-11-18 20:04:43 +0100

    Colour control layer

--- apps/midi/keymap.c
+++ apps/midi/keymap.c
@@ -11,9 +11,17 @@ void keyboard_post_init_user(void) {
 }
 #endif
 
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT(MI_C_1,  MI_F_1,  MI_B_1,
                MI_D_1,  MI_G_1,  MI_C_2,
-               MI_E_1,  MI_A_1,  MI_D_2)
+               LT(1,MI_E_1),  MI_A_1,  MI_D_2),
+
+  [1] = LAYOUT(RGB_HUI, RGB_VAI, RGB_MOD,
+               RGB_HUD, RGB_TOG, RGB_SAI,
+               _______, RGB_VAD, RGB_SAD),
+
+
 };
