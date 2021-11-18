060cd6dd6f6cc0c4c57e5399b26ce736434e2912
llvllch <mart@llvll.ch>
2021-11-18 14:25:30 +0100

    purely midi

--- apps/midi/keymap.c
+++ apps/midi/keymap.c
@@ -4,12 +4,7 @@ extern MidiDevice midi_device;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [0] = LAYOUT(MI_C, MI_D, MI_E,
-               MI_F, MI_G, MI_A,
-               LT(1,MI_A), MI_B, MI_C_1),
-
-  [1] = LAYOUT(RGB_HUI, RGB_VAI, RGB_MOD,
-               RGB_HUD, RGB_TOG, RGB_SAI,
-               _______, RGB_VAD, RGB_SAD),
-
+  [0] = LAYOUT(MI_C_1,  MI_F_1,  MI_B_1,
+               MI_D_1,  MI_G_1,  MI_C_2,
+               MI_E_1,  MI_A_1,  MI_D_2)
 };
