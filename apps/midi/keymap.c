e837656c2e07ae8bdb71eebb83484a7eda4f9fda
veebch <mart@veeb.ch>
2024-10-15 22:32:16 +0200

    added brightness

--- apps/midi/keymap.c
+++ apps/midi/keymap.c
@@ -1,23 +1,30 @@
-#include QMK_KEYBOARD_H
-
-extern MidiDevice midi_device;
-
+#include "quantum.h"
 
 #ifdef RGBLIGHT_ENABLE
 void keyboard_post_init_user(void) {
-  rgblight_enable_noeeprom(); // Enables RGB, without saving settings
-  rgblight_sethsv_noeeprom(HSV_GOLD);
-  rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-  rgblight_set_brightness(32); // Set brightness (0-255)
+    rgblight_enable();
+    rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT);
+    rgblight_sethsv(30, 255, 32);  // Set gold color with 25% brightness
 }
 #endif
 
+// Define the layout for a 9-key (3x3) macropad
+#define LAYOUT( \
+    K00, K01, K02, \
+    K10, K11, K12, \
+    K20, K21, K22  \
+) { \
+    { K00, K01, K02 }, \
+    { K10, K11, K12 }, \
+    { K20, K21, K22 }  \
+}
 
-
+// Define the keymap using the layout
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-  [0] = LAYOUT(MI_C_1,  MI_F_1,  MI_B_1,
-               MI_D_1,  MI_G_1,  MI_C_2,
-               MI_E_1,  MI_A_1,  MI_D_2)
-
+    [0] = LAYOUT(
+        MI_C1,  MI_F1,  MI_B1,
+        MI_D1,  MI_G1,  MI_C2,
+        MI_E1,  MI_A1,  MI_D2
+    )
 };
+
